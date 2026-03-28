b26716bf8616ed8d34ed377e63ff7f29ed57204e
nerun <danieldiasr@gmail.com>
2026-03-28 13:31:08 -0300

    Update (src/ls.php): better error handling, add "-l" listing option

--- src/ls.php
+++ src/ls.php
@@ -32,20 +32,92 @@ require_once __DIR__ . '/lib/locale.php';
 
 $currentLocale = setAppLocale();
 setlocale(LC_COLLATE, $currentLocale);
+setlocale(LC_TIME, $currentLocale);
+
+$rows = [];
+$validOptions = ['a', 'l'];
+const ERR_NO_SUCH_FILE = 0;
+const ERR_INVALID_OPTION = 1;
 
 // ====== Auxiliary functions ======
 
-function printError($name) {
-    echo "ls: cannot access '$name': No such file or directory<br>";
+function printError($error, $name) {
+    switch ($error) {
+        case 0:
+            echo "ls: cannot access '$name': No such file or directory<br>";
+            break;
+        case 1:
+            echo "ls: invalid option -- '$name'<br>";
+            echo "Try 'ls --help' for more information.<br>";
+            break;
+    }
 }
 
-function printName($name) {
+function printName($name, $path, $flags = []) {
+    global $rows;
+    
     if (strpos($name, ' ') !== false) {
-        echo "'$name'";
-    } else {
-        echo $name;
+        $name = "'$name'";
     }
-    echo "<br>";
+    
+    $longListing = in_array('l', $flags);
+
+    if ($longListing) {
+        $stat = stat($path);
+        $user = posix_getpwuid($stat['uid'])['name'];
+        $group = posix_getgrgid($stat['gid'])['name'];
+        $mtime = strftime("%b %d %Y %H:%M", filemtime($path));
+
+        $rows[] = [
+            'perms' => symbolicPerms($path),
+            'nlink' => $stat['nlink'],
+            'user' => $user,
+            'group' => $group,
+            'size' => $stat['size'],
+            'date' => $mtime,
+            'name' => $name
+        ];
+    } else {
+       $rows[] = $name;
+    }
+}
+
+function printList($flags = []) {
+    global $rows;
+    $longListing = in_array('l', $flags);
+    
+    if ($longListing) {
+        $maxUser = $maxGroup = $maxSize = 0;
+        
+        foreach ($rows as $r) {
+            if (!is_array($r)) continue;
+            
+            $maxUser = max($maxUser, strlen($r['user']));
+            $maxGroup = max($maxGroup, strlen($r['group']));
+            $maxSize = max($maxSize, strlen($r['size']));
+        }
+    }
+
+    echo "<pre>";
+    
+    foreach ($rows as $r) {
+        if ($longListing && is_array($r)) {
+            printf(
+                "%-10s %2s %-{$maxUser}s %-{$maxGroup}s %{$maxSize}s %s  %s<br>",
+                $r['perms'],
+                $r['nlink'],
+                $r['user'],
+                $r['group'],
+                $r['size'],
+                $r['date'],
+                $r['name']
+            );
+        } else {
+            printf("%s<br>", $r);
+        }
+    }
+    
+    echo "</pre>";
 }
 
 function listDirectory($path, $flags = []) {
@@ -57,15 +129,64 @@ function listDirectory($path, $flags = []) {
 
     foreach ($items as $item) {
         if (!$showHidden && $item[0] === '.') continue;
-        printName($item);
+        $fullpath = $path . DIRECTORY_SEPARATOR . $item;
+        printName($item, $fullpath, $flags);
     }
+}
+
+function symbolicPerms($path){
+    $perms = fileperms($path);
+    $info = match ($perms & 0xF000) {
+        0xC000 => 's', // socket
+        0xA000 => 'l', // symbolic link
+        0x8000 => '-', // regular file
+        0x6000 => 'b', // block special
+        0x4000 => 'd', // directory
+        0x2000 => 'c', // character special
+        0x1000 => 'p', // FIFO pipe
+        default => 'u', // unknown
+    };
+    
+    // Owner
+    $info .= (($perms & 0x0100) ? 'r' : '-');
+    $info .= (($perms & 0x0080) ? 'w' : '-');
+    $info .= (($perms & 0x0040) ?
+                (($perms & 0x0800) ? 's' : 'x' ) :
+                (($perms & 0x0800) ? 'S' : '-'));
+    
+    // Group
+    $info .= (($perms & 0x0020) ? 'r' : '-');
+    $info .= (($perms & 0x0010) ? 'w' : '-');
+    $info .= (($perms & 0x0008) ?
+                (($perms & 0x0400) ? 's' : 'x' ) :
+                (($perms & 0x0400) ? 'S' : '-'));
+    
+    // World
+    $info .= (($perms & 0x0004) ? 'r' : '-');
+    $info .= (($perms & 0x0002) ? 'w' : '-');
+    $info .= (($perms & 0x0001) ?
+                (($perms & 0x0200) ? 't' : 'x' ) :
+                (($perms & 0x0200) ? 'T' : '-'));
+    
+    return $info;
 }
 
 // ====== Main logic ======
 
-function ls($args = [], $flags = []) {
+function ls($args = [], $longFlags = [], $flags = []) {
+    global $rows;
+    global $validOptions;
     $files = [];
     $folders = [];
+    
+    $validSet = array_flip($validOptions);
+
+    foreach ($flags as $flag) {
+        if (!isset($validSet[$flag])) {
+            printError(ERR_INVALID_OPTION, $flag);
+            return false;
+        }
+    }
 
     if (empty($args)) {
         $folders['.'] = $_SESSION['cwd'];
@@ -74,7 +195,7 @@ function ls($args = [], $flags = []) {
             $rp = realpath($_SESSION['cwd'] . DIRECTORY_SEPARATOR . $item);
 
             if ($rp === false) {
-                printError($item);
+                printError(ERR_NO_SUCH_FILE, $item);
                 continue;
             }
 
@@ -88,27 +209,29 @@ function ls($args = [], $flags = []) {
 
     if (!empty($files)) {
         foreach ($files as $name => $path) {
-            printName($name);
+            printName($name, $path, $flags);
         }
     }
 
     if (!empty($folders)) {
         if (!empty($files)) {
-            echo "<br>";
+            $rows[] = '';
         }
 
         $last = array_key_last($folders);
 
         foreach ($folders as $name => $path) {
-            if (count($folders) > 1) {
-                echo "$name:<br>";
+            if (count($folders) + count($files) > 1) {
+                $rows[] = "$name:";
             }
 
             listDirectory($path, $flags);
 
             if ($name !== $last) {
-                echo "<br>";
+                $rows[] = '';
             }
         }
     }
+    
+    printList($flags);
 }
